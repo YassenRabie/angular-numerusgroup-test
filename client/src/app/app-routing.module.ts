48ed552bf45b9051226f8a9578c01cb146d5cebc
Yaseen <YaseenR@q-pros.com>
2024-01-27 03:00:55 +0300

    Update login route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,8 +6,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { ArticalDetailsComponent } from './pages/artical-details/artical-details.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: "login", component: LoginComponent },
+  { path: "", component: LoginComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "information/:id", component: InformationComponent },
   { path: "artical/:id", component: ArticalDetailsComponent },
